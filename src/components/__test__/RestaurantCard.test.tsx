2fb7e9be7f84891a861fc621390ce7e1d4b392b7
sjranju <sjranju@gmail.com>
2023-11-15 12:05:35 +0000

    recreated dist

--- src/components/__test__/RestaurantCard.test.tsx
+++ src/components/__test__/RestaurantCard.test.tsx
@@ -1,47 +1,43 @@
 import '../../setupTests'
-import { act, render } from "@testing-library/react"
+import { act, render, renderHook, waitFor } from "@testing-library/react"
 import BodyRQ from "../BodyRQ"
-import React from "react"
+import React, { JSXElementConstructor, ReactElement } from "react"
 import LoginOrSignup from "../../context/LoginOrSignup"
 import * as restaurantCard from '../mocks/restaurantCardMock.json'
 import * as TYPES from "../../utils/interfaces"
+import nock from 'nock'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { fetchRestaurants } from '../../utils/fetchRestaurantDetails'
 
-global.fetch = jest.fn(() => {
-    const response: Response = {
-        json: () => { return Promise.resolve(restaurantCard) },
-        headers: new Headers(),
-        ok: true,
-        redirected: false,
-        status: 200,
-        statusText: 'OK',
-        type: 'default',
-        url: '',
-        clone: function (): Response {
-            throw new Error('Function not implemented.')
-        },
-        body: null,
-        bodyUsed: false,
-        arrayBuffer: function (): Promise<ArrayBuffer> {
-            throw new Error('Function not implemented.')
-        },
-        blob: function (): Promise<Blob> {
-            throw new Error('Function not implemented.')
-        },
-        formData: function (): Promise<FormData> {
-            throw new Error('Function not implemented.')
-        },
-        text: function (): Promise<string> {
-            throw new Error('Function not implemented.')
-        }
-    }
-    return Promise.resolve(response)
-})
+type propsType = {
+    children: React.ReactElement<any, string | React.JSXElementConstructor<any>>;
+}
 
-it('should render restaurant card', () => {
-    render(
-        <LoginOrSignup>
-            <BodyRQ />
-        </LoginOrSignup>)
+it('should render Body component', async () => {
+    const queryClient = new QueryClient()
+    const wrapper = (props: propsType) => (
+        <QueryClientProvider client={queryClient}>
+            {props.children}
+        </QueryClientProvider>
+    )
 
-    const name = screen
+    const expectaion = nock('https://corsproxy.io/?https://www.swiggy.com')
+        .get('/dapi/restaurants/list/')
+        .query(true)
+        .reply(200, restaurantCard)
+
+    // render(
+    //     <QueryClientProvider client={queryClient}>
+    //         <LoginOrSignup>
+    //             <BodyRQ />
+    //         </LoginOrSignup>
+    //     </QueryClientProvider>
+    // )
+
+    // render(<BodyRQ/> , { wrapper })
+    await http.get('https://corsproxy.io/?https://www.swiggy.com')
+    // await waitFor(async () => { return result.current.then(res => res) })
+    expectaion.done()
+    // expect((await result.current).length).toBeGreaterThan(4)
+
 })
