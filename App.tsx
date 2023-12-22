1a103b896fe0d638a311790bd3021eaab9d6271f
sjranju <sjranju@gmail.com>
2023-12-22 18:48:08 +0000

    Added lazy loading

--- App.tsx
+++ App.tsx
@@ -1,27 +1,26 @@
 import React, { Suspense, lazy } from "react"
 import ReactDOM from "react-dom/client"
 import Header from "./src/components/Header"
-import { createBrowserRouter, RouterProvider, Outlet, createHashRouter } from "react-router-dom"
-import Help from "./src/components/Help"
+import { RouterProvider, Outlet, createHashRouter } from "react-router-dom"
 import Error from "./src/components/Error"
 import RestaurantMenu from "./src/components/RestaurantMenu"
 import './index.css'
 import { Provider } from "react-redux"
 import { store } from './src/store/appStore'
-import Cart from "./src/components/Cart"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import BodyRQ from "./src/components/BodyRQ"
 import { SkeletonTheme } from "react-loading-skeleton"
 import RestaurantContext from "./src/context/RestaurantContext"
-import Login from "./src/components/Login"
 import UserContext from "./src/context/UserContext"
 import ResetCartContext from "./src/context/ResetCartContext"
-import Search from "./src/components/Search"
 import Footer from "./src/components/Footer"
 import LoginOrSignup from "./src/context/LoginOrSignup"
 import FilterRestaurantsContext from "./src/context/FilterRestaurantsContext"
 
-const About = lazy(() => import('./src/components/Help'))
+const Help = lazy(() => import('./src/components/Help'))
+const Login = lazy(() => import('./src/components/Login'))
+const Cart = lazy(() => import('./src/components/Cart'))
+const Search = lazy(() => import('./src/components/Search'))
 const queryClient = new QueryClient()
 
 const App = () => {
@@ -59,13 +58,6 @@ const appRouter = createHashRouter([
                 element: <><BodyRQ /><Footer /></>
             },
             {
-                path: '/about',
-                element:
-                    <Suspense>
-                        <About />
-                    </Suspense>
-            },
-            {
                 path: '/contact',
                 element:
                     <Suspense>
@@ -78,15 +70,24 @@ const appRouter = createHashRouter([
             },
             {
                 path: '/cart',
-                element: <Cart />
+                element:
+                    <Suspense>
+                        <Cart />
+                    </Suspense>
             },
             {
                 path: '/login',
-                element: <Login />
+                element:
+                    <Suspense>
+                        <Login />
+                    </Suspense>
             },
             {
                 path: '/search',
-                element: <Search />
+                element:
+                    <Suspense>
+                        <Search />
+                    </Suspense>
             }
         ],
         errorElement: <Error />
