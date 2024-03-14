224f65073cc51e8b419fe68f93827da4578e3881
sjranju <sjranju@gmail.com>
2024-03-14 16:57:34 +0000

    Updated CorsProxy

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,16 +1,16 @@
-export const RESTAURANT_API = 'https://thingproxy.freeboard.io/fetch/' + encodeURIComponent('https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.979568962372062&lng=77.50290893018244&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING')
+export const RESTAURANT_API = 'https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.979568962372062&lng=77.50290893018244&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING'
 
 export const CLOUDINARY_URL = 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/'
 
-export const RESTAURANT_ITEM = 'https://thingproxy.freeboard.io/fetch/' + encodeURIComponent('https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.979568962372062&lng=77.50290893018244&restaurantId=')
+export const RESTAURANT_ITEM = 'https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.979568962372062&lng=77.50290893018244&restaurantId='
 
 export const COUPON_URL = 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_28,h_28/'
 
 export const TOP_PICKS_URL = 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_628,h_704/TopPicks/'
 
-export const GET_MORE_RESTAURANTS = 'https://corsproxy.io/?' + encodeURIComponent('https://www.swiggy.com/dapi/restaurants/list/update')
+export const GET_MORE_RESTAURANTS = 'https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/update'
 
-export const PRE_SEARCH = 'https://thingproxy.freeboard.io/fetch/' + encodeURIComponent('https://www.swiggy.com/dapi/landing/PRE_SEARCH?lat=12.979568962372062&lng=77.50290893018244')
+export const PRE_SEARCH = 'https://corsproxy.io/?https://www.swiggy.com/dapi/landing/PRE_SEARCH?lat=12.979568962372062&lng=77.50290893018244'
 
 export const PRE_SEARCH_CLOUDINARY_URL = 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/'
 
